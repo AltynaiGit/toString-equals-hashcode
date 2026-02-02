0e537f44bba6255cf4a910cd69adbf223a4387e5
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-02 18:37:03 +0600

    task 1 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     static void main() {
+        Person p1 = new Person("Trump", 80);
+        Person p2 = new Person("Trump", 80);
+        Person p3 = new Person("Putin", 70);
 
+        System.out.println(p1);
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.equals(p3));
+        System.out.println(p1.hashCode());
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() { //выводит сам объект а не ссылку
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
