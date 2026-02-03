d54c1f93b5a245f91d9cae37b850404a467b6808
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-03 18:52:36 +0600

    task-4-feature/max-element-from-random-array commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
 public class Main {
     static void main() {
+        int[] num = new int[3];
+        Random rnd = new Random();
+        int max = Integer.MIN_VALUE;
 
+        for (int i = 0; i < num.length; i++) {
+            num[i] = rnd.nextInt(3);
+            if (num[i] > max) {
+                max = num[i];
+
+            }
+        }
+        System.out.println("Random: " + Arrays.toString(num) + ". Max is " + max);
     }
-
 }
