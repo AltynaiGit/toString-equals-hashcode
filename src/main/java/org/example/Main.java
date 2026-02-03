f40add05750ab198f105a8888eb467cb96f453f5
AltynaiGit <altynai.777.777.777@gmail.com>
2026-02-03 19:32:43 +0600

    task-5-binary-search-find-index commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
 package org.example;
 
-public class Main {
-    static void main() {
+   import java.util.Arrays;
 
-    }
+        public class Main {
+            public static void main(String[] args) {
+                int[] arr = {1, 4, 2, 5, 2, 5};
+                Arrays.sort(arr);
+                int target = 4;
 
-}
+                int index = binarySearch(arr, target);
+                System.out.println(index);
+            }
+
+            public static int binarySearch(int[] arr, int target) {
+                int low = 0;
+                int high = arr.length - 1;
+
+                while (low <= high) {
+                    int mid = low + (high - low) / 2;
+                    if (arr[mid] == target) return mid;
+                    if (arr[mid] < target) low = mid + 1;
+                    else high = mid - 1;
+                }
+                return -1;
+            }
+        }
